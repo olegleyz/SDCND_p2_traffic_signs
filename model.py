5813f94f59952ed07036ab572f9c52dcb2a8f01d
Oleg Leyzerov <olegleyz@gmail.com>
2017-03-10 15:25:53 +0100

    change evaluate in model2

--- model.py
+++ model.py
@@ -82,7 +82,16 @@ def model_processing(mode, X_train_pp, y_train, X_validation, y_validation,  X_t
     correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
     accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     saver = tf.train.Saver()
-
+    def evaluate(X_data, y_data):
+            BATCH_SIZE = 128
+            num_examples = len(X_data)
+            total_accuracy = 0
+            sess = tf.get_default_session()
+            for offset in range(0, num_examples, BATCH_SIZE):
+                batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
+                accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y, keep_prob: 1})
+                total_accuracy += (accuracy * len(batch_x))
+            return total_accuracy / num_examples
     if mode==1:
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
@@ -107,13 +116,4 @@ def model_processing(mode, X_train_pp, y_train, X_validation, y_validation,  X_t
     elif mode==2:
         return evaluate(X_test, y_test)
 
-    def evaluate(X_data, y_data):
-        BATCH_SIZE = 128
-        num_examples = len(X_data)
-        total_accuracy = 0
-        sess = tf.get_default_session()
-        for offset in range(0, num_examples, BATCH_SIZE):
-            batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
-            accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y, keep_prob: 1})
-            total_accuracy += (accuracy * len(batch_x))
-        return total_accuracy / num_examples
+    
